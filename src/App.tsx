0ffd34654d8c4a7a8e2ee0a0a2c308124e754af5
siva2204 <sivaraman26r@gmail.com>
2021-09-30 15:15:26 +0530

    [Add] React boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Heading } from '@chakra-ui/react';
+
+function App() {
+  return <Heading>hello world from snowpack and chakra!!</Heading>;
+}
+
+export default App;
